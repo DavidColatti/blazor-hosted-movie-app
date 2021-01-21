e699c79c0dbd892bb2cee179cee10776e781afe7
David Colatti <davidcolatti@gmail.com>
2021-01-21 15:18:43 -0500

    feat: initial commit and basic set up

---
+++ Client/Helpers/StringUtilities.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string CustomToUpper(string value) => value.ToUpper();
+    }
+}
