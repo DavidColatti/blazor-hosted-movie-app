e28a37dd5ddb3a88f1cc3ad6bd3df35d92ec5b48
David Colatti <davidcolatti@gmail.com>
2021-02-06 16:24:08 -0500

    feat: dependency injection

---
+++ Client/Services.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazorMovies.Client
+{
+    public class SingletonService
+    {
+       public int Value { get; set; }
+    }
+
+    public class TransientService
+    {
+        public int Value { get; set; }
+    }
+}
