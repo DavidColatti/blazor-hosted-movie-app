53e04170f65cfb87b223994522b4e35c90c2c3d1
David Colatti <davidcolatti@gmail.com>
2021-02-19 15:47:38 -0500

    feat: partial classes

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorMovies.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value += 1;
+            transient.Value += 1;
+        }
+    }
+}
